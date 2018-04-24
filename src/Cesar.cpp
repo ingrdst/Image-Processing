f357d1f7d02752de0b561e465f9b0df2e460fc17
Ingrid Soares <ingr.dcg@gmail.com>
2018-04-24 09:51:58 +0000

    Leitura caracteres

--- src/Cesar.cpp
+++ src/Cesar.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -13,16 +14,39 @@ Cesar::Cesar(){
 
 void Cesar::aplicarCriptoPGM(ImagemPGM & img){
 	ImagemPGM* imagempgm = &img;
-    int  key=0;
-	char buf;
+
+
+    //Valores RGB
     int x,z;
+	char rgb[50];
+    int i=0,j=0;
+    string key;
+    
+    //Percorre imagem
+    for(x= 0; x < imagempgm->getLargura(); ++x){
+       for(z= 0; z < imagempgm->getAltura(); ++z){
+        while(rgb[i]!='\0'){
+		key[j]=rgb[i];
+		j++;
+		i++;
+		}
+		key[j]='\0';
+        }
+    }
+   
+
+	char buf;
+	int cesar;
+
+	cout << "Valor= ";
+	cin >> cesar;
 
     //Cifra
     for(x= 0; x < imagempgm->getLargura(); ++x){
       for(z= 0; z < imagempgm->getAltura(); ++z){
 		if(buf>='A'&&buf<='Z'){
            	buf-='A';
-			buf+=26-key;
+			buf+=26-cesar;
 			buf%=26;
 			buf+='a';
            	}
