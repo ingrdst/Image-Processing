76263941169ecaf3a81be3a9f08a71efdd98ffab
Ingrid Soares <ingr.dcg@gmail.com>
2018-04-24 04:47:00 -0300

    Declaracao da criptografia tipo PPM

---
+++ inc/CriptoPPM.hpp
@@ -0,0 +1,17 @@
+#ifndef CRIPTOPPM_HPP
+#define CRIPTOPPM_HPP
+#include "Imagem.hpp"
+
+using namespace std;
+
+class CriptoPPM{
+
+  public:
+    CriptoPPM();
+    ~CriptoPPM();
+
+    void aplicarCriptoPPM(ImagemPPM & imagemppm);
+
+};
+
+#endif
